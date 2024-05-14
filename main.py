d4a6ef563ee122db759811f1f417ebd4b5c16f38
RMJames123 <rm.james@hotmail.com>
2024-05-14 05:18:56 +0000

    Funcion get

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
 from flask import Flask, jsonify, request
 
+app = Flask(__name__)
 
-@app.route('/')
+@app.route("/")
 def route():
    return 'Hola mundo'
 
-app = Flask(__name__)
+@app.route("/users/<user_id>")
+def get_user(user_id):
+   user = {"id":user_id,"name": "Raul", "telefono": "999-888-777"}
+   query = request.args.get("query")
+   if query:
+      user["query"] = query
+   return jsonify(user),200
+
 
 if __name__ == "__main__":
    app.run(debug=True)

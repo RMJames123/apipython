2694a9ca33608dc13013dc4e51974f2043a57f00
RMJames123 <rm.james@hotmail.com>
2024-05-14 05:34:05 +0000

    metodo post

--- main.py
+++ main.py
@@ -15,5 +15,11 @@ def get_user(user_id):
    return jsonify(user),200
 
 
+@app.route("/users", methods = ['POST'])
+def create_user():
+   data = request.get_json()
+   data["status"] = "user created"
+   return jsonify(data), 201
+
 if __name__ == "__main__":
    app.run(debug=True)
